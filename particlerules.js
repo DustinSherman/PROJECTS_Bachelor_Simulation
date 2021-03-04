af98638d53a5374fc6a3f775a248a89efa094e6f
Dustin Sherman <dustin.sherman@yahoo.de>
2021-03-04 15:55:08 +0100

    UPDATE

--- particlerules.js
+++ particlerules.js
@@ -7,6 +7,7 @@
 
 let fm = 0;
 let fe = 1;
+let ff = 2;
 let cas = 3;
 let cac = 4;
 let caa = 5;
@@ -15,16 +16,16 @@ let particleResults = [
     // Results FluidMovement (FM) / FluidExplosion (FE) / Fluidflowfield (FF) / CA Neighbourhood/Rule Set (CAS) / CA Complete (CAC) / CA Animate (CAA)
 
     // Lowest Akin / State	| 1   | -1  | 2   | -2  | 3   | -3  | 4   | -4  | 5   | -5  | 6   | -6  | 7   | -7  | 8   | -8  | 9   | -9  | 10  | -10 | 11  | -11 |
-    /* 0 */                 [ fm  , fm  , fm  , fm  , fm  , fm  , fe  , fe  , cac , cac , fm  , cac ,     ,     , fe  , caa ,     ,     ,     ,     ,     ,     ],
-    /* 1 */                             [ fm  , fm  , fm  , fm  , fm  , fm  , fe  , fe  ,     ,     , fe  , caa , fe  , cac , fe  , caa , fe  , caa , fe  , caa ],
-    /* 2 */                                         [ fm  , fm  , fm  , fm  , cas , cas ,     ,     ,     ,     , fm  , cas ,     ,     , fe  , cac , fe  , cac ],
-    /* 3 */                                                     [ fm  , fm  , fm  , fm  ,     ,     ,     ,     , fm  , cas ,     ,     , fm  , cas ,     ,     ],
-    /* 4 */                                                                 [ fm  , fm  , fm  , cas , fe  , caa , fm  , cas ,     ,     , fm  , cas ,     ,     ],
-    /* 5 */                                                                             [ fe  , cac ,     ,     , fe  , caa , fe  , caa , fm  , cas ,     ,     ],
-    /* 6 */                                                                                         [     ,     , fe  , cac ,     ,     , fe  , caa , fe  , caa ],
-    /* 7 */                                                                                                     [     ,     ,     ,     , fe  , cac , fe  , cac ],
-    /* 8 */                                                                                                                 [     ,     ,     ,     ,     ,     ],
-    /* 9 */                                                                                                                             [     ,     ,     ,     ],
+    /* 0 */                 [ fm  , fm  , fm  , fm  , fm  , fm  , fe  , fe  , cac , cac , fm  , fm  , fe  , fe  , fe  , fe  , caa , caa ,     ,     , fe  , fe  ],
+    /* 1 */                             [ fm  , fm  , fm  , fm  , fm  , fm  , fm  , fm  , fm  , fm  , cas , cas , cac , cac , cac , cac , fe  , fe  , caa , caa ],
+    /* 2 */                                         [ fm  , fm  , fm  , fm  , cas , cas ,     ,     ,     ,     ,     ,     , fm  , fm  , caa , caa , cac , cac ],
+    /* 3 */                                                     [ fm  , fm  , fm  , fm  , fm  , fm  ,     ,     ,     ,     ,     ,     , cac , cac , cas , cas ],
+    /* 4 */                                                                 [ fm  , fm  , fm  , fm  ,     ,     ,     ,     ,     ,     , cas , cas ,     ,     ],
+    /* 5 */                                                                             [ fe  , fe  , cac , cac , cas , cas , fm  , fm  ,     ,     ,     ,     ],
+    /* 6 */                                                                                         [ fe  , fe  , cac , cac , cas , cas , cas , cas ,     ,     ],
+    /* 7 */                                                                                                     [     ,     , cac , cac , cac , cac , cas , cas ],
+    /* 8 */                                                                                                                 [ caa , caa , fe  , fe  , caa , caa ],
+    /* 9 */                                                                                                                             [     ,     , fe  , fe  ],
     /* 10 */                                                                                                                                        [     ,     ]
 ]
 
